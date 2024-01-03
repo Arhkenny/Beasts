b99bc73432c16d666280127b1eaeedc960c0f585
anton.frantsuzau <mystudy17.12@gmail.com>
2024-01-03 08:15:58 +0300

    MAc 2

--- src/animals.kt
+++ src/animals.kt
@@ -69,7 +69,7 @@ fun main(){
     vet.giveShot(wolf)
     vet.giveShot(hippo)
     println("${wolf.hunger}")
-    println("Учу Git")
+
     println("From Mac")
     println("add new Extra commit ")
 
